5312b71dc0859a7a4031898d7b2e8940243d942e
Carloscastro16 <andycastro.001016@gmail.com>
2023-03-14 22:04:51 -0500

    ADD: Probando clima

--- src/app/clima/clima.page.ts
+++ src/app/clima/clima.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
+import { WeatherResponse } from 'src/app/interfaces/weather';
+import { Subject } from 'rxjs';
+import { AuthenticationService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.page.html',
-  styleUrls: ['./clima.page.scss'],
+  styleUrls: ['./clima.page.scss',  '../app.component.scss'],
 })
 export class ClimaPage implements OnInit {
 
-  constructor() { }
+  localizacion = 'Cancún, MX';
+  @Input() actualizarPrediccion: Subject<boolean> = new Subject<boolean>();
+  tiempo!:WeatherResponse;
+  imagen_resumen!:string;
+  ahora:Date = new Date();
+  constructor(
+    private weatherService:WeatherService,
+    public authService: AuthenticationService
+    ) {
 
-  ngOnInit() {
+  }
+  
+  ngOnInit(): void {
+    console.debug('Estamos en On Init de Weather component');
+    this.cargarPrediccion();
+
+    this.actualizarPrediccion.subscribe(response => {
+      console.log("Holi!");
+      this.cargarPrediccion();
+    });
   }
 
+    private handleError(error: any) { 
+      let errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg);
+    }
+  
+    private cargarPrediccion(){
+      this.weatherService.getWeather(this.localizacion).subscribe( resp => {
+        console.debug('Parece que recibo algo en el on Init de TAB1');
+        this.ahora = new Date();
+        this.tiempo = resp;
+        this.imagen_resumen = `https://openweathermap.org/img/wn/${this.tiempo.weather[0].icon}@2x.png`;
+        console.log(resp);
+      },
+      err => {
+        this.handleError(err);
+      });
+    }
 }
---
+++ src/app/interfaces/common.ts
@@ -0,0 +1,15 @@
+
+export interface Clouds {
+    all: number;
+}
+
+export interface Wind {
+    speed: number;
+    deg: number;
+  }
+
+  
+  export interface Coord {
+    lat: number;
+    lon: number;
+  }
