f03479deff5e84de5aa03f596080e9af5ccda023
Carloscastro16 <andycastro.001016@gmail.com>
2023-03-14 22:05:17 -0500

    Revert "ADD: Probando clima"
    This reverts commit 5312b71dc0859a7a4031898d7b2e8940243d942e.

--- src/app/clima/clima.page.ts
+++ src/app/clima/clima.page.ts
@@ -1,54 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { WeatherService } from 'src/app/services/weather.service';
-import { WeatherResponse } from 'src/app/interfaces/weather';
-import { Subject } from 'rxjs';
-import { AuthenticationService } from '../services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.page.html',
-  styleUrls: ['./clima.page.scss',  '../app.component.scss'],
+  styleUrls: ['./clima.page.scss'],
 })
 export class ClimaPage implements OnInit {
 
-  localizacion = 'Cancún, MX';
-  @Input() actualizarPrediccion: Subject<boolean> = new Subject<boolean>();
-  tiempo!:WeatherResponse;
-  imagen_resumen!:string;
-  ahora:Date = new Date();
-  constructor(
-    private weatherService:WeatherService,
-    public authService: AuthenticationService
-    ) {
+  constructor() { }
 
-  }
-  
-  ngOnInit(): void {
-    console.debug('Estamos en On Init de Weather component');
-    this.cargarPrediccion();
-
-    this.actualizarPrediccion.subscribe(response => {
-      console.log("Holi!");
-      this.cargarPrediccion();
-    });
+  ngOnInit() {
   }
 
-    private handleError(error: any) { 
-      let errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-      console.error(errMsg);
-    }
-  
-    private cargarPrediccion(){
-      this.weatherService.getWeather(this.localizacion).subscribe( resp => {
-        console.debug('Parece que recibo algo en el on Init de TAB1');
-        this.ahora = new Date();
-        this.tiempo = resp;
-        this.imagen_resumen = `https://openweathermap.org/img/wn/${this.tiempo.weather[0].icon}@2x.png`;
-        console.log(resp);
-      },
-      err => {
-        this.handleError(err);
-      });
-    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,9 +12,7 @@ export const environment = {
     storageBucket: "jardinsm51castro.appspot.com",
     messagingSenderId: "760628352117",
     appId: "1:760628352117:web:4e11ed8fefddce8a90543c"
-  },
-  apiKey: 'b91fe7d356210fc0fc1e02747a05c159',
-  owUrl: 'https://api.openweathermap.org/data/2.5'
+  }
 };
 
 /*
